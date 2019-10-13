17cb7788dab7216deb1503856dd40d52fe059b45
KiwookJUNG <ismeeki17@naver.com>
2019-10-13 22:49:57 +0900

    Initial Commit

---
+++ CodeView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeView
+//
+//  Created by 정기욱 on 2019/10/13.
+//  Copyright © 2019 kiwook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
